537f98cc5a7df7037cab3c33e6c6463562025058
Михаил Синяков <pvpshoot@gmail.com>
2019-01-09 16:44:09 +0400

    Updates

---
+++ precache-manifest.3153767d4600fdf2bac468e5433f714b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b51e1f7ba6177e1f9f83",
+    "url": "/couponManager/static/js/runtime~main.b51e1f7b.js"
+  },
+  {
+    "revision": "72cd5433699224cafe29",
+    "url": "/couponManager/static/js/main.72cd5433.chunk.js"
+  },
+  {
+    "revision": "30cb5305d0093f2499c2",
+    "url": "/couponManager/static/js/1.30cb5305.chunk.js"
+  },
+  {
+    "revision": "72cd5433699224cafe29",
+    "url": "/couponManager/static/css/main.244e5786.chunk.css"
+  },
+  {
+    "revision": "adc3173474d948cf3751d71a194af84e",
+    "url": "/couponManager/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/couponManager/precache-manifest.4b945510c079dc7af74fe2996777f59b.js"
+  "/couponManager/precache-manifest.3153767d4600fdf2bac468e5433f714b.js"
 );
 
 workbox.clientsClaim();
