6ec1b82e2b6ef890b5fb8403161ff9969b3e3543
Михаил Синяков <pvpshoot@gmail.com>
2019-01-09 16:04:41 +0400

    TEST

--- src/index.js
+++ src/index.js
@@ -1,48 +1,50 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider, connect } from "react-redux";
-import store from "./store";
-import App from "./App.jsx";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { Provider, connect } from "react-redux";
+// import store from "./store";
+// import App from "./App.jsx";
 import "./ecwidConfig";
-import * as serviceWorker from "./serviceWorker";
-import i18n from "i18next";
-import { withI18n, reactI18nextModule } from "react-i18next";
-import Backend from "i18next-xhr-backend";
+// import * as serviceWorker from "./serviceWorker";
+// import i18n from "i18next";
+// import { withI18n, reactI18nextModule } from "react-i18next";
+// import Backend from "i18next-xhr-backend";
 
-i18n
-  .use(Backend)
-  .use(reactI18nextModule)
-  .init({
-    fallbackLng: "en",
-    debug: true,
-    backend: {
-      loadPath: `locales/{{lng}}/{{ns}}.json`
-    },
-    interpolation: {
-      escapeValue: false // not needed for react as it escapes by default
-    },
-    react: {
-      wait: true
-    }
-  });
+// i18n
+//   .use(Backend)
+//   .use(reactI18nextModule)
+//   .init({
+//     fallbackLng: "en",
+//     debug: true,
+//     backend: {
+//       loadPath: `locales/{{lng}}/{{ns}}.json`
+//     },
+//     interpolation: {
+//       escapeValue: false // not needed for react as it escapes by default
+//     },
+//     react: {
+//       wait: true
+//     }
+//   });
 
-class CouponManager extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <Provider store={store}>
-        <App />
-      </Provider>
-    );
-  }
-}
+// class CouponManager extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {};
+//   }
+//   render() {
+//     return (
+//       <Provider store={store}>
+//         <App />
+//       </Provider>
+//     );
+//   }
+// }
 
-ReactDOM.render(<CouponManager />, document.getElementById("root"));
+// ReactDOM.render(<CouponManager />, document.getElementById("root"));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: http://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
+alert('IT WORKS')
