94a8112ad8fe36c7c13a4aadcc01b49dd863360d
Михаил Синяков <pvpshoot@gmail.com>
2019-01-09 15:38:25 +0400

    Updates

--- precache-manifest.362bc2b82a77d5742ce02e457fbebfd3.js
+++ precache-manifest.362bc2b82a77d5742ce02e457fbebfd3.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/couponManager/static/js/runtime~main.b51e1f7b.js"
   },
   {
-    "revision": "16ced724c762e62f83aa",
-    "url": "/couponManager/static/js/main.16ced724.chunk.js"
+    "revision": "07d9e9d804fbf50b2f95",
+    "url": "/couponManager/static/js/main.07d9e9d8.chunk.js"
   },
   {
     "revision": "0e1ac8c2a9487fa37daf",
     "url": "/couponManager/static/js/1.0e1ac8c2.chunk.js"
   },
   {
-    "revision": "16ced724c762e62f83aa",
+    "revision": "07d9e9d804fbf50b2f95",
     "url": "/couponManager/static/css/main.92f74f75.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/couponManager/static/css/1.4060b60d.chunk.css"
   },
   {
-    "revision": "4bf6176fb6fde011d3a23f1bee04a8e0",
+    "revision": "f65bdf4279716ca5217864edf6524a77",
     "url": "/couponManager/index.html"
   }
 ];
